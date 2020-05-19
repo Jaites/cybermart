c18f88558ef446ece18f1e7c3c1a9bb51549e655
Jaites <jupadim.ac@gmail.com>
2020-05-19 16:07:48 +0100

    Initial Commit -CyberMart eShop.

---
+++ cybermart/src/main/java/com/jaites/cybermart/CybermartApplication.java
@@ -0,0 +1,13 @@
+package com.jaites.cybermart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybermartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybermartApplication.class, args);
+	}
+
+}
